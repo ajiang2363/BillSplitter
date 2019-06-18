1a95505d66423c33d57b2272d0608a652afbf39b
Aaron Jiang <aj6566@rit.edu>
2019-06-18 13:25:46 -0400

    xml edited some. Added in userinfo and services

--- app/src/main/java/com/example/billsplitter/MainActivity.java
+++ app/src/main/java/com/example/billsplitter/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.billsplitter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signIn;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.HomePage);
+        setContentView(R.layout.homepage);
+        signIn = (Button)findViewById(R.id.SignInButton);
+        signUp = (Button)findViewById(R.id.SignUpButton);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInPageLoad();
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpPageLoad();
+            }
+        });
     }
+
+    public void signInPageLoad(){
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
+    }
+
+    public void signUpPageLoad(){
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+
+
 }
